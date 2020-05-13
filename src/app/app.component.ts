54891ce062e37433701b3d66244f6c56ee3c07c5
davebaol <davix7@yahoo.it>
2020-05-13 17:28:06 +0200

    Improve form.io json schema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,11 @@ import { JsonEditorOptions } from 'ang-jsoneditor';
 import form from './initial-form.json';
 import schema from './schema-root.json';
 import componentSchema from './schema-component.json';
+import logicSchema from './schema-logic.json';
 
 class JsonEditorOptions2 extends JsonEditorOptions {
-  schemaRefs = {};
+  schemaRefs = null;
+  onValidationError = null;
 }
 
 @Component({
@@ -23,7 +25,14 @@ export class AppComponent {
     this.jsonEditorOptions.mode = 'view'; // set default mode
     this.jsonEditorOptions.onError = (error) => console.log('jsonEditorOptions.onError:', error);
     this.jsonEditorOptions.schema = schema;
-    this.jsonEditorOptions.schemaRefs = {component: componentSchema};
+    this.jsonEditorOptions.schemaRefs = {
+      component: componentSchema,
+      logic: logicSchema
+    };
+    this.jsonEditorOptions.onValidationError = (errors: any[]) => {
+      console.log('Found', errors.length, 'validation errors:');
+      errors.forEach((error) => console.log(error));
+    };
     this.form = form;
   }
 
