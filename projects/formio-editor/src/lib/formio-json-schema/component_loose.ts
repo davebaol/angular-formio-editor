0be81e7ca64f8ba923a7567b5d61e17a76a4792c
davebaol <davix7@yahoo.it>
2020-05-15 17:47:15 +0200

    Fix lint errors

--- projects/formio-editor/src/lib/formio-json-schema/component_loose.ts
+++ projects/formio-editor/src/lib/formio-json-schema/component_loose.ts
@@ -1,4 +1,3 @@
-import component from "./component_strict"
-  
-// tslint:disable:object-literal-key-quotes quotemark semicolon
-export default Object.assign({}, component, { required: ["type"] });
+import component from './component_strict';
+
+export default Object.assign({}, component, { required: ['type'] });
