aba7d5d0d586f48874f212154685cdbbff0fde2b
davebaol <davix7@yahoo.it>
2020-05-24 22:58:46 +0200

    Add formio config provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { FormioModule } from 'angular-formio';
-import { FormioEditorModule } from '@davebaol/formio-editor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormioModule, FormioAppConfig } from 'angular-formio';
+import { FormioEditorModule } from '@davebaol/formio-editor';
+import { AppComponent } from './app.component';
+import { AppConfig } from '../formio-config';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: FormioAppConfig, useValue: AppConfig }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/formio-config.ts
@@ -0,0 +1,9 @@
+export const AppConfig = {
+    // See https://github.com/formio/angular-formio/blob/master/src/formio.config.ts
+
+    apiUrl: 'http://localhost/api',
+    appUrl: 'http://localhost/app',
+
+    // icons: '...',
+    // formOnly: false
+};
