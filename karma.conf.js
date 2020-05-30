1fa2e12349d9b4d832a7ae409c1a5f9f92db3381
davebaol <davix7@yahoo.it>
2020-05-30 15:39:27 +0200

    Rename demo project

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/AngularFormioEditor'),
+      dir: require('path').join(__dirname, './coverage/formio-editor-demo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
